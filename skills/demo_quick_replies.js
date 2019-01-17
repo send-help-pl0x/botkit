1c333523d7f65f0701f167268f2ef5f253152992
buitim <buitim@oregonstate.edu>
2019-01-17 09:23:40 -0800

    Derp search/replace fuckup

--- skills/demo_quick_replies.js
+++ skills/demo_quick_replies.js
@@ -1,11 +1,10 @@
-module.exports = function(controller) {
+module.exports = function (controller) {
 
-  controller.hears('quick replies','message_received', function(bot, message) {
+    controller.hears('quick replies', 'message_received', function (bot, message) {
 
-    bot.reply(message, {
-        text: 'Look, quick replies!',
-        quick_replies: [
-            {
+        bot.reply(message, {
+            text: 'Look, quick replies!',
+            quick_replies: [{
                 title: 'Hello',
                 payload: 'hello'
             },
@@ -13,10 +12,10 @@ module.exports = function(controller) {
                 title: 'Test',
                 payload: 'test'
             },
-        ]
-      },function() {});
+            ]
+        }, function () {});
 
 
-      });
+    });
 
 }
---
+++ skills/testing.js
@@ -0,0 +1,46 @@
+module.exports = function (controller) {
+    controller.hears("urmomgay", "message_received", function (bot, message) {
+        bot.reply(message, {
+            text: "no u",
+            typingDelay: 2500,
+            quick_replies: [{
+                title: "LMAO",
+                payload: "lmao"
+            },
+            {
+                title: "I wanna die lmao",
+                payload: "die"
+            }
+            ]
+        });
+    });
+
+    controller.hears("lmao", "message_received", function (bot, message) {
+        bot.reply(message, {
+            text: "no u",
+            typingDelay: 2500
+        });
+    });
+
+    controller.hears("die", "message_received", function (bot, message) {
+        bot.reply(message, {
+            text: "no u",
+            typingDelay: 2500
+        });
+    });
+
+    controller.hears("typing", "message_received", function (bot, message) {
+        bot.reply(
+            message, {
+                text: "This message used the automatic typing delay",
+                typing: true
+            },
+            function () {
+                bot.reply(message, {
+                    text: "This message specified a 5000ms typing delay",
+                    typingDelay: 5000
+                });
+            }
+        );
+    });
+};
