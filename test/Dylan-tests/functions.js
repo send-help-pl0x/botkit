11e2669b7464905e37973d8414dae13f05b8882c
Dylan Albertazzi <albertad@oregonstate.edu>
2019-01-27 17:47:49 -0800

    git checkout master

---
+++ test/Dylan-tests/functions.js
@@ -0,0 +1,10 @@
+const functions = {
+    add: function(num1, num2) {
+        return num1 + num2;
+    },
+
+    isNull: () => null
+
+};
+
+module.exports = functions;
---
+++ test/Dylan-tests/functions.test.js
@@ -0,0 +1,9 @@
+const functions = require('./functions');
+
+test('Adds 2 + 2 to equal 4', () => {
+    expect(functions.add(2, 2)).toBe(4);
+});
+
+test('Should be null', () => {
+    expect(functions.isNull()).toBeNull();
+});
