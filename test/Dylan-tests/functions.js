78c24ae3bb665e9068b9ab5a438ddbeca2e1b453
Dylan Albertazzi <albertad@oregonstate.edu>
2019-01-29 09:08:31 -0800

    push all changes

--- test/Dylan-tests/functions.js
+++ test/Dylan-tests/functions.js
@@ -1,10 +1,21 @@
+const axios = require('axios');
 const functions = {
     add: function(num1, num2) {
         return num1 + num2;
     },
 
-    isNull: () => null
+    createUser: () => {
+        const user = {
+            firstName: 'Dylan'
+        }
+        user['lastName'] = 'Albertazzi';
+        return user;
+    },
 
+    fetchUser: () => axios
+    .get('https://jsonplaceholder.typicode.com/users/1')
+    .then(res => res.data)
+    .catch(err => 'error')
 };
 
 module.exports = functions;
