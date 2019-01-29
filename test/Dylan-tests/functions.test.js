935250350b09c4c999e933d5155be97603fbeca7
Dylan Albertazzi <albertad@oregonstate.edu>
2019-01-29 09:08:54 -0800

    commiting changes

--- test/Dylan-tests/functions.test.js
+++ test/Dylan-tests/functions.test.js
@@ -4,6 +4,21 @@ test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2, 2)).toBe(4);
 });
 
-test('Should be null', () => {
-    expect(functions.isNull()).toBeNull();
+
+
+
+test('User should be DYlan', () => {
+    expect(functions.createUser()).toEqual({
+        firstName: 'Dylan',
+        lastName: 'Albertazzi'
+    });
 });
+
+//async data
+
+test('user name should me Leanne Graham', () => {
+    expect.assertions(1);
+    return functions.fetchUser().then(data => {
+        expect(data.name).toEqual('Leanne Graham');
+    })
+})
