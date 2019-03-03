541986d7dbfb544be24dccfb4b7acdbe14fb1236
buitim <buitim@oregonstate.edu>
2019-03-03 00:32:03 -0800

    changed things...

--- test/facebook/Personas_profile_api.test.js
+++ test/facebook/Personas_profile_api.test.js
@@ -1,6 +1,8 @@
-const {facebookbot} = require('../../lib/Botkit');
+const {
+    facebookbot
+} = require('../../lib/Botkit');
 
-describe('Personas_profile_api', () => {
+describe('== Personas_profile_api', () => {
 
     test('Should not fail', () => {
         expect(1).toBeTruthy();
