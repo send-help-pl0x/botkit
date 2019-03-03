46427af7a7ad596a3e5db3bb013fe16ea114eb17
buitim <buitim@oregonstate.edu>
2019-03-03 01:27:22 -0800

    pay no attention to the man behind the curtain

--- test/facebook/Personas_profile_api.test.js
+++ test/facebook/Personas_profile_api.test.js
@@ -3,7 +3,7 @@ const {
 } = require('../../lib/Botkit');
 
 describe('== Personas_profile_api', () => {
-
+    const controller = botkit.core({});
     test('Should not fail', () => {
         expect(1).toBeTruthy();
     });
---
+++ test/fuckthisshit/Web.test.js
@@ -0,0 +1,11 @@
+const {
+    webbot
+} = require('../../lib/Web');
+
+describe('== send help plz', () => {
+
+    test('This should pass i think', () => {
+        expect(webbot).toBeTruthy();
+    });
+
+});
