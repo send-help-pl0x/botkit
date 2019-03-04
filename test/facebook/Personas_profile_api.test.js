1da13eca87d1a594f8c96d04f284eba0e6194dc1
buitim <buitim@oregonstate.edu>
2019-03-03 16:56:53 -0800

    RYAN SUCKS

--- test/facebook/Personas_profile_api.test.js
+++ test/facebook/Personas_profile_api.test.js
@@ -3,7 +3,6 @@ const {
 } = require('../../lib/Botkit');
 
 describe('== Personas_profile_api', () => {
-    const controller = botkit.core({});
     test('Should not fail', () => {
         expect(1).toBeTruthy();
     });
