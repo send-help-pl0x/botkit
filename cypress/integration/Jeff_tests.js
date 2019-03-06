3134abb24f00cc3556ce73b8f4660af488de3bd2
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2019-03-05 18:16:46 -0800

    Added Jeff tests for Jeff

--- cypress/integration/Jeff_tests.js
+++ cypress/integration/Jeff_tests.js
@@ -55,5 +55,73 @@ describe('Jeff test boi', function(){
         cy.get(':nth-child(10) > .message > p')
         .should('contain', 'I do not know how to respond to that message yet.')
     })
+
+    it('[+] From "help" conversation, type \'test\'', function(){
+        cy.wait(2000)
+        
+        cy.get('input[id="messenger_input"]')
+          .type('help')
+          .should('have.value', 'help')
+
+        cy.contains('Send').click()
+
+        cy.wait(2000)
+        
+        cy.get('input[id="messenger_input"]')
+          .type('test')
+          .should('have.value', 'test')
+
+        cy.contains('Send').click()
+
+        cy.get(':nth-child(12) > .message > p')
+          .should('contain', 'I heard a test')
+    })
+
+    it('[+] After "help" conversation, type \'test\'', function(){
+        cy.wait(2000)
+        
+        cy.get('input[id="messenger_input"]')
+          .type('help')
+          .should('have.value', 'help')
+
+        cy.contains('Send').click()
+
+        cy.wait(2000)
+        
+        cy.get('input[id="messenger_input"]')
+          .type('test')
+          .should('have.value', 'test')
+
+        cy.contains('Send').click()
+
+        cy.wait(2000)
+        cy.get('input[id="messenger_input"]')
+          .type('test')
+          .should('have.value', 'test')
+
+        cy.contains('Send').click()
+
+        cy.get(':nth-child(20) > .message > p')
+          .should('contain', 'I heard a test')
+    })
+
+    it('[+] After quick response, type \'test\'', function(){
+        cy.wait(2000)
+        
+        cy.get('input[id="messenger_input"]')
+          .type('urmomgay')
+          .should('have.value', 'urmomgay')
+
+        cy.contains('Send').click()
+        
+        cy.get('input[id="messenger_input"]')
+          .type('test')
+          .should('have.value', 'test')
+
+        cy.contains('Send').click()
+
+        cy.get(':nth-child(23) > .message > p')
+          .should('contain', 'I heard a test')
+    })
     
 })
