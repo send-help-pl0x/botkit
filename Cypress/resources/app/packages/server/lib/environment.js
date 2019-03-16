784b7d546fda5c79eb2b2017ea33ee44095fb8aa
jeffdeng1314 <32289287+jeffdeng1314@users.noreply.github.com>
2019-03-16 16:07:42 -0700

    Refactored some CoreBot.js codes

---
+++ Cypress/resources/app/packages/server/lib/environment.js
@@ -0,0 +1,44 @@
+(function() {
+  var Promise, app, base, config, cwd, env, os, pkg, ref;
+
+  require("./util/http_overrides");
+
+  require("./util/fs");
+
+  os = require("os");
+
+  cwd = require("./cwd");
+
+  Promise = require("bluebird");
+
+  Error.stackTraceLimit = 2e308;
+
+  pkg = require("@packages/root");
+
+  try {
+    app = require("electron").app;
+    app.commandLine.appendSwitch("disable-renderer-backgrounding", true);
+    app.commandLine.appendSwitch("ignore-certificate-errors", true);
+    app.commandLine.appendSwitch("use-fake-ui-for-media-stream");
+    app.commandLine.appendSwitch("use-fake-device-for-media-stream");
+    app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
+    if (os.platform() === "linux") {
+      app.disableHardwareAcceleration();
+    }
+  } catch (error) {}
+
+  env = (base = process.env)["CYPRESS_ENV"] || (base["CYPRESS_ENV"] = (ref = pkg.env) != null ? ref : "development");
+
+  config = {
+    cancellation: true
+  };
+
+  if (env === "dev") {
+    config.longStackTraces = true;
+  }
+
+  Promise.config(config);
+
+  module.exports = env;
+
+}).call(this);
